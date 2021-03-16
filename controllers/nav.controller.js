41e691229fb539627621a87afdd9178ce2365fb7
Matthew <matthew.t.showman@gmail.com>
2021-03-16 14:50:30 -0500

    Updated the getProductList controller based to include the IscurrentProd flag

--- controllers/nav.controller.js
+++ controllers/nav.controller.js
@@ -36,7 +36,7 @@ async function getProductList(ClientID){
         let pool = await sql.connect(config);
         let item = await pool.request()
             .input('IdParam', sql.Int, ClientID)
-            .query('SELECT i.ItemID, i.ItemName, i.BrandID, b.BrandName, i.ClientID, c.ClientName, i.CatID, pc.CatName, i.GroupID, pg.GroupName, i.CurrentProfile, p.ProfileName FROM Item i JOIN Brand b ON i.BrandID = b.BrandID JOIN Client c ON i.ClientID = c.ClientID JOIN ProductCategory pc ON i.CatID = pc.CatID JOIN ProductGroup pg ON i.GroupID = pg.GroupID JOIN SeasonalProfile p ON i.CurrentProfile = p.ProfileID WHERE i.ClientID = @IdParam');
+            .query('SELECT i.ItemID, i.ItemName, i.BrandID, b.BrandName, i.ClientID, c.ClientName, i.CatID, pc.CatName, i.GroupID, pg.GroupName, i.CurrentProfile, p.ProfileName, i.IsCurrentProd FROM Item i JOIN Brand b ON i.BrandID = b.BrandID JOIN Client c ON i.ClientID = c.ClientID JOIN ProductCategory pc ON i.CatID = pc.CatID JOIN ProductGroup pg ON i.GroupID = pg.GroupID JOIN SeasonalProfile p ON i.CurrentProfile = p.ProfileID WHERE i.ClientID = @IdParam');
         return item.recordsets;
     }
     catch (error) {
