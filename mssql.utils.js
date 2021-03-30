d654dfea43785f48836a2091ee9eff15bd8b52b7
MatthewShowman <matthew.t.showman@gmail.com>
2021-03-30 14:01:02 -0500

    Saved the utils to connect to the real DB

--- mssql.utils.js
+++ mssql.utils.js
@@ -9,11 +9,11 @@ When I used to use MongoDB (and mongoose), I had to create these.
  */
 
 const mssqlConfig = {
-/*     user: 'ISYS5943HG',
+    user: 'ISYS5943HG',
     password: 'GohogsUA1!',
     server: 'essql1.walton.uark.edu',
-    database: 'ISYS5943HG', */
-        user: 'SA',
+    database: 'ISYS5943HG',
+    /*    user: 'SA',
         password: 'practiceDB123',
         server: 'localhost',
         database: 'TestDB',
