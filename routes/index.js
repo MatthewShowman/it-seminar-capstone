3a5c6ecb282ac1abb062802ddf4ac1e15b7c4966
MatthewShowman <matthew.t.showman@gmail.com>
2021-03-31 09:00:28 -0500

    Updated the routes to match the new controller structure

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,23 @@
 const router = require('express').Router();
-const navController = require('../controllers/nav.controller');
-const forecastController = require('../controllers/forecast.controller');
+const readController = require('../controllers/read.controller');
 const updateController = require('../controllers/update.controller');
+const createController = require('../controllers/create.controller');
 
 const WMWeekServices = require('../services/wm-week.service');
 const ProfileServices = require('../services/profile.service');
 const ForecastServices = require('../services/forecast.service');
 const HistoricalServices = require('../services/historical.service');
 
-// Navigation Routes
+
+
+
+
+// ******************** READ ROUTES ********************
 
 // Route to READ all clients
 // Returns a an array of all client objects
 router.route('/clients').get((req, res) => {
-    navController.getClientList().then(result => {
+    readController.getClientList().then(result => {
         res.json(result);
     })
 })
@@ -21,7 +25,7 @@ router.route('/clients').get((req, res) => {
 // Route to READ all brands for one client
 // Returns an array of all brand objects for a given client.
 router.route('/clients/:id').get((req, res) => {
-    navController.getBrandList(req.params.id).then(result => {
+    readController.getBrandList(req.params.id).then(result => {
         res.json(result);
     })
 })
@@ -29,7 +33,32 @@ router.route('/clients/:id').get((req, res) => {
 // Route to READ all products from one brand for one client
 // Returns an array [array of product objects, array of profiles]
 router.route('/products/:id').get((req, res) => {
-    navController.getProductList(req.params.id).then(result => {
+    readController.getProductList(req.params.id).then(result => {
+        res.json(result);
+    })
+})
+
+// Route to READ all profiles available for one client
+// Returns an array [array of profiles]
+router.route('/profiles/:id').get((req, res) => {
+    readController.getProfileList(req.params.id).then(result => {
+        res.json(result);
+    })
+})
+
+// Route to READ hostorical data for one item
+// Returns an array of all history objects for one item
+router.route('/products/historical-data/:id').get((req, res) => {
+    readController.getHistoricalData(req.params.id).then(result => {
+        console.log(result);
+        res.json(result);
+    })
+})
+
+// Route to READ one year of forecast data for one object
+// Returns an array of 52 weekly forecast objects
+router.route('/products/forecast/:id').get((req, res) => {
+    readController.getItemForecast(req.params.id).then(result => {
         res.json(result);
     })
 })
@@ -37,55 +66,49 @@ router.route('/products/:id').get((req, res) => {
 // Route to get to the add item page
 // Returns an array with info to help populate the new item (i.e. brand, category, and group)
 router.route('/products/add/:id').get((req, res) => {
-    navController.addItemGetter(req.params.id).then(result => {
-        res.json(result);
-    })
-})
-
-router.route('/profiles/:id').get((req, res) => {
-    navController.getProfileList(req.params.id).then(result => {
-        res.json(result);
-    })
-})
-
-
-// Forecast Data Routes
-
-// Route to READ one year of forecast data for one object
-// Returns an array of 52 weekly forecast objects
-router.route('/products/forecast/:id').get((req, res) => {
-    forecastController.getItemForecast(req.params.id).then(result => {
-        res.json(result);
-    })
-})
-
-
-// Historical Data Route
-
-// Route to READ hostorical data for one item
-// Returns an array of all history objects for one item
-router.route('/products/historical-data/:id').get((req, res) => {
-    navController.getHistoricalData(req.params.id).then(result => {
-        console.log(result);
+    readController.addItemGetter(req.params.id).then(result => {
         res.json(result);
     })
 })
 
 
 
-// Functional Routes (Adding and Updating)
 
-// ITEMS
 
-// Add a new product and create the initial forecast
-router.route('/products/add/').post((req, res) => {
-    let newItemObj = { ...req.body };
-    updateController.addItem(newItemObj).then(result => {
+// ******************** CREATE ROUTES ********************
+
+// CREATE a new client record and create the initial forecast
+router.route('/clients/add/').post((req, res) => {
+    let newClient = req.body.ClientName;
+    createController.addClient(newClient).then(result => {
         res.status(201).json(result);
     })
 })
 
-// Update product-level edits
+// CREATE a new product record and create the initial forecast
+router.route('/products/add/').post((req, res) => {
+    let newItemObj = { ...req.body };
+    createController.addItem(newItemObj).then(result => {
+        res.status(201).json(result);
+    })
+})
+
+// CREATE (1) SeasonalProfile record and (2) 52 weeks of ProfileData
+router.route('/profiles/add/').post((req, res) => {
+    let profile = req.body.profile;
+    let profileData = req.body.profileData;
+    createController.createNewProfile(profile, profileData).then(result => {
+        res.json(result);
+    })
+})
+
+
+
+
+
+// ******************** UPDATE ROUTES ********************
+
+// UPDATE product-level edits
 router.route('/products/update/').post((req, res) => {
     let itemObj = { ...req.body };
     updateController.updateItemInfo(itemObj).then(result => {
@@ -93,7 +116,7 @@ router.route('/products/update/').post((req, res) => {
     })
 })
 
-// Update weekly-forecast-level changes
+// UPDATE weekly-forecast-level changes
 router.route('/products/forecast/update/').post((req, res) => {
     let forecastObj = { ...req.body };
     updateController.updateItemForecast(forecastObj).then(result => {
@@ -101,16 +124,15 @@ router.route('/products/forecast/update/').post((req, res) => {
     })
 })
 
-// PROFILES
 
-// Add (1) SeasonalProfile record and (2) 52 weeks of ProfileData
-router.route('/profiles/add/').post((req, res) => {
-    let profile = req.body.profile;
-    let profileData = req.body.profileData;
-    updateController.createNewProfile(profile, profileData).then(result => {
-        res.json(result);
-    })
-})
+
+
+
+// ******************** DELETE ROUTES ********************
+
+// This functionality will not be built into this initial MVP
+
+
 
 
 
