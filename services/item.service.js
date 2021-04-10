2bfe2447614160acc409c7991bb7fc13e8df11e7
MatthewShowman <matthew.t.showman@gmail.com>
2021-04-10 18:37:20 -0500

    Updated services to retunr ClientID

--- services/item.service.js
+++ services/item.service.js
@@ -7,18 +7,18 @@ async function getCompleteItemList(ClientID) {
         let item = await pool.request()
             .input('IdParam', sql.Int, ClientID)
             .query('DECLARE @WeekCode INT ' +
-                    'SET @WeekCode = (SELECT WMweekCode FROM WMWeek WHERE CalStartDate BETWEEN DATEADD(week,-1,GETDATE()) AND GETDATE()) ' +
-                    'SELECT i.ItemID, i.ItemName, i.BrandID, b.BrandName, i.ClientID, c.ClientName, i.CatID, ' +
-                        'pc.CatName, i.GroupID, pg.GroupName, i.CurrentProfile, p.ProfileName, i.DefaultPrice, ' +
-                        'f.ForecastStores AS CurrentStores, i.IsCurrentProd ' +
-                    'FROM Item i ' +
-                        'JOIN Brand b ON i.BrandID = b.BrandID ' +
-                        'JOIN Client c ON i.ClientID = c.ClientID ' +
-                        'JOIN ProductCategory pc ON i.CatID = pc.CatID ' +
-                        'JOIN ProductGroup pg ON i.GroupID = pg.GroupID ' +
-                        'JOIN SeasonalProfile p ON i.CurrentProfile = p.ProfileID ' +
-                        'LEFT JOIN Forecast f ON i.ItemID = f.ItemID AND f.WMWeekCode = @WeekCode ' +
-                    'WHERE i.ClientID = @IdParam;');
+                'SET @WeekCode = (SELECT WMweekCode FROM WMWeek WHERE CalStartDate BETWEEN DATEADD(week,-1,GETDATE()) AND GETDATE()) ' +
+                'SELECT i.ItemID, i.ItemName, i.BrandID, b.BrandName, i.ClientID, c.ClientName, i.CatID, ' +
+                'pc.CatName, i.GroupID, pg.GroupName, i.CurrentProfile, p.ProfileName, i.DefaultPrice, ' +
+                'f.ForecastStores AS CurrentStores, i.IsCurrentProd ' +
+                'FROM Item i ' +
+                'JOIN Brand b ON i.BrandID = b.BrandID ' +
+                'JOIN Client c ON i.ClientID = c.ClientID ' +
+                'JOIN ProductCategory pc ON i.CatID = pc.CatID ' +
+                'JOIN ProductGroup pg ON i.GroupID = pg.GroupID ' +
+                'JOIN SeasonalProfile p ON i.CurrentProfile = p.ProfileID ' +
+                'LEFT JOIN Forecast f ON i.ItemID = f.ItemID AND f.WMWeekCode = @WeekCode ' +
+                'WHERE i.ClientID = @IdParam;');
         return item.recordsets[0];
     }
     catch (error) {
@@ -34,15 +34,15 @@ async function getSingleItem(ItemID) {
             .query('DECLARE @WeekCode INT ' +
                 'SET @WeekCode = (SELECT WMweekCode FROM WMWeek WHERE CalStartDate BETWEEN DATEADD(week,-1,GETDATE()) AND GETDATE()) ' +
                 'SELECT i.ItemID, i.ItemName, i.BrandID, b.BrandName, i.ClientID, c.ClientName, i.CatID, ' +
-                    'pc.CatName, i.GroupID, pg.GroupName, i.CurrentProfile, p.ProfileName, i.DefaultPrice, ' +
-                    'f.ForecastStores AS CurrentStores, i.IsCurrentProd ' +
+                'pc.CatName, i.GroupID, pg.GroupName, i.CurrentProfile, p.ProfileName, i.DefaultPrice, ' +
+                'f.ForecastStores AS CurrentStores, i.IsCurrentProd ' +
                 'FROM Item i ' +
-                    'JOIN Brand b ON i.BrandID = b.BrandID ' +
-                    'JOIN Client c ON i.ClientID = c.ClientID ' +
-                    'JOIN ProductCategory pc ON i.CatID = pc.CatID ' +
-                    'JOIN ProductGroup pg ON i.GroupID = pg.GroupID ' +
-                    'JOIN SeasonalProfile p ON i.CurrentProfile = p.ProfileID ' +
-                    'LEFT JOIN Forecast f ON i.ItemID = f.ItemID AND f.WMWeekCode = @WeekCode ' +
+                'JOIN Brand b ON i.BrandID = b.BrandID ' +
+                'JOIN Client c ON i.ClientID = c.ClientID ' +
+                'JOIN ProductCategory pc ON i.CatID = pc.CatID ' +
+                'JOIN ProductGroup pg ON i.GroupID = pg.GroupID ' +
+                'JOIN SeasonalProfile p ON i.CurrentProfile = p.ProfileID ' +
+                'LEFT JOIN Forecast f ON i.ItemID = f.ItemID AND f.WMWeekCode = @WeekCode ' +
                 'WHERE i.ItemID = @IdParam;');
         return item.recordsets[0];
     }
@@ -61,11 +61,11 @@ async function createNewItem(newItemJSON) {
             .input('ClientID', sql.Int, newItemJSON.ClientID)
             .input('CatID', sql.Int, newItemJSON.CatID)
             .input('GroupID', sql.Int, newItemJSON.GroupID)
-            .input('DefaultPrice', sql.Decimal(6,2), newItemJSON.DefaultPrice)
+            .input('DefaultPrice', sql.Decimal(6, 2), newItemJSON.DefaultPrice)
             .input('CurrentProfile', sql.Int, newItemJSON.CurrentProfile)
             .query('INSERT INTO Item (ItemName, BrandID, ClientID, CatID, GroupID, DefaultPrice, CurrentProfile) ' +
-                    'OUTPUT inserted.ItemID ' +
-                    'VALUES (@ItemName, @BrandID, @ClientID, @CatID, @GroupID, @DefaultPrice, @CurrentProfile)');
+                'OUTPUT inserted.ItemID ' +
+                'VALUES (@ItemName, @BrandID, @ClientID, @CatID, @GroupID, @DefaultPrice, @CurrentProfile)');
         return insertItem.recordset[0].ItemID;
     }
     catch (error) {
@@ -82,13 +82,13 @@ async function updateItem(ItemInfoObj) {
             .input('BrandID', sql.VarChar, ItemInfoObj.BrandID)
             .input('CatID', sql.Int, ItemInfoObj.CatID)
             .input('GroupID', sql.Int, ItemInfoObj.GroupID)
-            .input('DefaultPrice', sql.Decimal(6,2), ItemInfoObj.DefaultPrice)
+            .input('DefaultPrice', sql.Decimal(6, 2), ItemInfoObj.DefaultPrice)
             .input('CurrentProfile', sql.Int, ItemInfoObj.CurrentProfile)
             .query('UPDATE Item ' +
-                    'SET ItemName = @ItemName, ' +
-                        'BrandID = @BrandID, CatID = @CatID, GroupID = @GroupID, ' +
-                        'DefaultPrice = @DefaultPrice, CurrentProfile = @CurrentProfile ' +
-                    'WHERE ItemID = @ItemID');
+                'SET ItemName = @ItemName, ' +
+                'BrandID = @BrandID, CatID = @CatID, GroupID = @GroupID, ' +
+                'DefaultPrice = @DefaultPrice, CurrentProfile = @CurrentProfile ' +
+                'WHERE ItemID = @ItemID');
         return updateItem;
     }
     catch (error) {
@@ -97,8 +97,8 @@ async function updateItem(ItemInfoObj) {
 }
 
 module.exports = {
-    getCompleteItemList : getCompleteItemList,
-    getSingleItem : getSingleItem,
-    createNewItem : createNewItem,
-    updateItem : updateItem
+    getCompleteItemList: getCompleteItemList,
+    getSingleItem: getSingleItem,
+    createNewItem: createNewItem,
+    updateItem: updateItem
 }
